0f53fb5ded0bd7b1c3f33b3ec29b662bb4b11d42
Adrian Llarena <adrianllahe17@gmail.com>
2025-06-30 21:10:58 +0200

    Create aplicacion multiplataforma de Supermercados

---
+++ src/module-info.java
@@ -0,0 +1,16 @@
+module ExamenAdrianLLarena {
+	requires javafx.controls;
+	requires java.desktop;
+	requires javafx.web;
+	requires javafx.fxml;
+	requires javafx.swing;
+	requires javafx.media;
+	requires javafx.graphics;
+	requires javafx.base;
+	requires java.sql;
+	requires org.kordamp.ikonli.javafx;
+	
+	opens application to javafx.graphics, javafx.fxml;
+	opens controller to javafx.graphics, javafx.fxml;
+	opens model to javafx.graphics, javafx.fxml, javafx.base;
+}
